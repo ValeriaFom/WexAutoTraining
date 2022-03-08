75419e6f13474e3cfac3c3d1384359ce963ec68c
Valeria Klimentieva <klimentsyeva_val@mail.ru>
2022-03-08 20:37:47 +0300

    added main folder and HW4

---
+++ Homework4/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Homework 4
+            //1.Создать два произвольных массива, подсчитать максимальное  и минимальное значение их элементов. Вывести на экран.
+            //2.Поменять в массивах максимальные и минимальные значения местами
+
+            int[] array1 = { 5, -7, 56, 1, 23 };
+
+            int minValue = array1[0];
+            int minPlace = 0;
+
+            for (int i = 1; i < array1.Length; i++)
+            {
+                if (array1[i] < minValue)
+                {
+                    minValue = array1[i];
+                    minPlace = i;
+                }
+            }
+
+            int maxValue = array1[0];
+            int maxPlace = 0;
+
+            for (int i = 1; i < array1.Length; i++)
+            {
+                if (array1[i] > maxValue)
+                {
+                    maxValue = array1[i];
+                    maxPlace = i;
+                }
+            }
+
+            foreach (int item in array1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("minValue and place = {0}, {1}, maxValue and place = {2}, {3}", minValue, minPlace, maxValue, maxPlace);
+
+            int oldMinPlace = array1[minPlace];
+            array1[minPlace] = array1[maxPlace];
+            array1[maxPlace] = oldMinPlace;
+
+            foreach (int item in array1)
+            {
+                Console.WriteLine(item);
+            }
+
+            int[] array2 = { 98465, 3, 0, -45, 1568 };
+
+            int minValue2 = array2[0];
+            int minPlace2 = 0;
+
+            for (int i = 1; i < array2.Length; i++)
+            {
+                if (array2[i] < minValue2)
+                {
+                    minValue2 = array2[i];
+                    minPlace2 = i;
+                }
+            }
+
+            int maxValue2 = array2[0];
+            int maxPlace2 = 0;
+
+            for (int i = 1; i < array2.Length; i++)
+            {
+                if (array2[i] > maxValue2)
+                {
+                    maxValue2 = array2[i];
+                    maxPlace2 = i;
+                }
+            }
+
+            foreach (int item in array2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("minValue and place = {0}, {1}, maxValue and place = {2}, {3}", minValue2, minPlace2, maxValue2, maxPlace2);
+
+            int oldMinPlace2 = array2[minPlace2];
+            array2[minPlace2] = array2[maxPlace2];
+            array2[maxPlace2] = oldMinPlace2;
+
+            foreach (int item in array2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ WexAutoTraining/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace WexAutoTraining
+{
+    class Program
+    {
+        static void CreateArray(int[] array, int degree)
+        { 
+            for (int index = 0; index < array.Length; index++)
+            {
+                array[index] = (int)Math.Pow(index, degree);               
+            }
+        }
+
+        static float GetAverage(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i]; // sum = sum + array[i]
+            }
+            return (sum / array.Length);
+        }
+
+        static void Main(string[] args)
+        {
+            //Homework 1
+            //Вывести в консоль максимальные и минимальные допустимые значения для типов данных byte, short, int, long, float, double, uint, ulong.
+
+            //byte byteMin = byte.MinValue;
+            //byte byteMax = byte.MaxValue;
+
+            //Console.WriteLine("Min and Max values for byte = {0} {1}", byteMin, byteMax);
+
+            //short shortMin = short.MinValue;
+            //short shortMax = short.MaxValue;
+
+            //Console.WriteLine("Min and Max values for short = {0} {1}", shortMin, shortMax);
+
+            //int intMin = int.MinValue;
+            //int intMax = int.MaxValue;
+
+            //Console.WriteLine("Min and Max values for int = {0} {1}", intMin, intMax);
+
+            ////long, float, double, uint, ulong.
+
+            //uint uintMin = uint.MinValue;
+            //uint uintMax = uint.MaxValue;
+
+            //Console.WriteLine("Min and Max values for uint = {0} {1}", uintMin, uintMax);
+
+            //long longMin = long.MinValue;
+            //long longMax = long.MaxValue;
+
+            //Console.WriteLine("Min and Max values for long = {0} {1}", longMin, longMax);
+
+            //ulong ulongMin = ulong.MinValue;
+            //ulong ulongMax = ulong.MaxValue;
+
+            //Console.WriteLine("Min and Max values for ulong = {0} {1}", ulongMin, ulongMax);
+
+            //float floatMin = float.MinValue;
+            //float floatMax = float.MaxValue;
+
+            //Console.WriteLine("Min and Max values for float = {0} {1}", floatMin, floatMax);
+
+            //double doubleMin = double.MinValue;
+            //double doubleMax = double.MaxValue;
+
+            //Console.WriteLine("Min and Max values for double = {0} {1}", doubleMin, doubleMax);
+
+
+            //Homework 2
+            //Создать массив размером 10 элементов, заполнить его квадратами индексов (в цикле), и вывести его содержимое на экран
+
+            //int[] squared = new int[10];
+
+            //for (int index = 0; index < squared.Length; index++)
+            //{
+            //    squared[index] = index * index;
+            //    Console.WriteLine(index + " element = " + squared[index]);
+            //}
+
+            //Homework 3
+            //Cоздать три массива по 10 элементов, первый заполнить значениями индексов элементов, второй - квадратами индексов, третий-кубами, и вывести на экран средние значения элементов для всех массивов
+
+            int[] indexes = new int[10];
+            CreateArray(indexes, 1);
+            foreach (int item in indexes)
+            {
+                Console.WriteLine("Element = " + item);
+            }
+            Console.WriteLine("Average = " + GetAverage(indexes));
+
+            int[] squared = new int[10];
+            CreateArray(squared, 2);
+            foreach (int item in squared)
+            {
+                Console.WriteLine("Element = " + item);
+            }
+            Console.WriteLine("Average = " + GetAverage(squared));
+
+            int[] cubed = new int[10];
+            CreateArray(cubed, 3);
+            foreach (int item in cubed)
+            {
+                Console.WriteLine("Element = " + item);
+            }
+            Console.WriteLine("Average = " + GetAverage(cubed));
+
+            //Console.WriteLine(index + " element = " + array[index]);
+
+            //int[] marks = new int[10];
+            //for (int i = 1; i <= 3; i++)
+            //{
+            //    CreateArray(marks, 1);
+            //    Console.WriteLine(GetAverage(marks));
+            //}
+
+        }
+    }
+}
