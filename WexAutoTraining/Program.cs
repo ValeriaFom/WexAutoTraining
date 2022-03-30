b5d85c1c49f83378b263dad2893a91c0f6c62ea5
Valeria Klimentieva <klimentsyeva_val@mail.ru>
2022-03-30 23:38:43 +0300

    Delete unnecessary comments

--- WexAutoTraining/Program.cs
+++ WexAutoTraining/Program.cs
@@ -9,46 +9,6 @@ namespace Homework1_Min_MaxValues
             //Homework 1
             //Вывести в консоль максимальные и минимальные допустимые значения для типов данных byte, short, int, long, float, double, uint, ulong.
 
-            //byte byteMin = byte.MinValue;
-            //byte byteMax = byte.MaxValue;
-
-            //Console.WriteLine("Min and Max values for byte = {0} {1}", byteMin, byteMax);
-
-            //short shortMin = short.MinValue;
-            //short shortMax = short.MaxValue;
-
-            //Console.WriteLine("Min and Max values for short = {0} {1}", shortMin, shortMax);
-
-            //int intMin = int.MinValue;
-            //int intMax = int.MaxValue;
-
-            //Console.WriteLine("Min and Max values for int = {0} {1}", intMin, intMax);
-
-            //uint uintMin = uint.MinValue;
-            //uint uintMax = uint.MaxValue;
-
-            //Console.WriteLine("Min and Max values for uint = {0} {1}", uintMin, uintMax);
-
-            //long longMin = long.MinValue;
-            //long longMax = long.MaxValue;
-
-            //Console.WriteLine("Min and Max values for long = {0} {1}", longMin, longMax);
-
-            //ulong ulongMin = ulong.MinValue;
-            //ulong ulongMax = ulong.MaxValue;
-
-            //Console.WriteLine("Min and Max values for ulong = {0} {1}", ulongMin, ulongMax);
-
-            //float floatMin = float.MinValue;
-            //float floatMax = float.MaxValue;
-
-            //Console.WriteLine("Min and Max values for float = {0} {1}", floatMin, floatMax);
-
-            //double doubleMin = double.MinValue;
-            //double doubleMax = double.MaxValue;
-
-            //Console.WriteLine("Min and Max values for double = {0} {1}", doubleMin, doubleMax);
-
             Console.WriteLine($"Min and Max values for byte = {byte.MinValue} {byte.MaxValue}");
             Console.WriteLine($"Min and Max values for short = {short.MinValue} {short.MaxValue}");
             Console.WriteLine($"Min and Max values for int = {int.MinValue} {int.MaxValue}");
